f6d241404120d4262f7382435c8e2d24f44a83a3
5nuff <intechnic.l.r@gmail.com>
2019-11-04 22:48:24 -0500

    scoreboard

--- public/main.js
+++ public/main.js
@@ -1,7 +1,62 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const main = () => {}
+
+let resultRight = 0
+let resultLeft = 0
+
+const addItTeamRight = () => {
+  // resultRight = defaultNumberRight++
+  resultRight++
+  document.querySelector('.team-2-score').textContent = resultRight
 }
 
+const addItTeamLeft = () => {
+  // resultLeft = defaultNumberLeft++
+  resultLeft++
+  document.querySelector('.team-1-score').textContent = resultLeft
+}
+
+const subtractItTeamRight = () => {
+  // resultRight = defaultNumberRight--
+  resultRight--
+  document.querySelector('.team-2-score').textContent = resultRight
+}
+
+const subtractItTeamLeft = () => {
+  // resultLeft = defaultNumberLeft--
+  resultLeft--
+  document.querySelector('.team-1-score').textContent = resultLeft
+}
+
+const team1Name = () => {
+  const team1Add = document.querySelector('.team-1-input').value
+  console.log(team1Add)
+  document.querySelector('.team-1-name').textContent = team1Add
+}
+const team2Name = () => {
+  const team2Add = document.querySelector('.team-2-input').value
+  console.log(team2Add)
+  document.querySelector('.team-2-name').textContent = team2Add
+}
 document.addEventListener('DOMContentLoaded', main)
+
+document
+  .querySelector('.update-team-1-name')
+  .addEventListener('click', team1Name)
+
+document
+  .querySelector('.update-team-2-name')
+  .addEventListener('click', team2Name)
+
+document
+  .querySelector('.team-1-add-1-button')
+  .addEventListener('click', addItTeamLeft)
+document
+  .querySelector('.team-1-subtract-1-button')
+  .addEventListener('click', subtractItTeamLeft)
+document
+  .querySelector('.team-2-add-1-button')
+  .addEventListener('click', addItTeamRight)
+
+document
+  .querySelector('.team-2-subtract-1-button')
+  .addEventListener('click', subtractItTeamRight)
